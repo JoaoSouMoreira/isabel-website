37e40485c893a22ae364b7e558cf5561fad7870f
Joao Moreira <joaomoreira@curative.com>
2025-07-25 23:04:15 -0400

    Fix warnings and add selected style to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
       <h1 className="navbar-title">Isabel Castanho.</h1>
       <ul>
         {routes.map((route) => (
-          <li key={route.path}><a href={route.path}>{route.name}</a></li>
+          <li key={route.path}><a className={window.location.pathname === route.path ? 'active' : ''} href={route.path}>{route.name}</a></li>
         ))}
       </ul>
     </nav>
